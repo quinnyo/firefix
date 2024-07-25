198e1aa906525ed4eac9f469aadbd03d77370add
quinnyo <3379314+quinnyo@users.noreply.github.com>
2024-07-26 06:58:42 +1000

    browser.compactmode.show
    Restore the 'compact' toolbar density option.

--- user.js
+++ user.js
@@ -3,3 +3,9 @@
 // :basic:UI:accessibility:
 // Disable trackpad(?) gesture history navigation.
 user_pref("widget.disable-swipe-tracker", true);
+
+// 2024-07-26
+// :UI:tweaks:
+// Restore the 'compact' toolbar density option.
+user_pref("browser.compactmode.show", true);
+
