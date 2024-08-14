513ad6ee878c7158c8da19587c35909cc0575fa9
quinnyo <3379314+quinnyo@users.noreply.github.com>
2024-08-14 22:31:22 +1000

    browser.urlbar.autoFill
    Allow navigating to manually typed URLs with Enter.

--- user.js
+++ user.js
@@ -9,3 +9,7 @@ user_pref("widget.disable-swipe-tracker", true);
 // Restore the 'compact' toolbar density option.
 user_pref("browser.compactmode.show", true);
 
+// 2024-08-14
+// Allow navigating to manually typed URLs with Enter.
+user_pref("browser.urlbar.autoFill", false);
+
